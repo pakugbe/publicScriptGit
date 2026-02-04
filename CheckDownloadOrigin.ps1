aa34244bc3b9a530775b09f060a9525a29acc4ed
ViktorPer <vperhati@tegos.eu>
2026-02-04 12:07:31 +0100

    Update CheckDownloadOrigin.ps1
    Signed-off-by: ViktorPer <vperhati@tegos.eu>

--- CheckDownloadOrigin.ps1
+++ CheckDownloadOrigin.ps1
@@ -2,23 +2,35 @@ $downloadsPath = "$env:USERPROFILE\Downloads"
 $schoolPath = "$env:USERPROFILE\Documents\Schule"
 $schoolDomains = @('https://rbbk-do.de/', 'https://schulendortmund.taskcards.app/')
 
+$folderMap = @{
+    'Englisch' = 'ENG'
+    'EVP' = 'EVP'
+    'GIT-T' = 'GIT-T'
+    'PGL' = 'PGL'
+    'STD' = 'STD'
+    'gitwbl' = 'GITWBL'
+}
+
 Get-ChildItem -Path $downloadsPath -File | ForEach-Object {
-    $shouldMove = $false
-    
     try {
         $stream = Get-Content -Path $_.FullName -Stream Zone.Identifier -ErrorAction Stop
         $urls = ($stream | Select-String -Pattern '(HostUrl|ReferrerUrl)=(.*)$').Matches.Groups[2].Value
         
         if ($urls | Where-Object { $url = $_; $schoolDomains | Where-Object { $url -like "$_*" } }) {
-            Write-Host "$($_.Name) Found (Domain Match)" -ForegroundColor Green
-            $shouldMove = $true
+            $targetFolder = $null
+            
+            foreach ($key in $folderMap.Keys) {
+                if ($urls -match $key) {
+                    $targetFolder = Join-Path $schoolPath $folderMap[$key]
+                    break
+                }
+            }
+            
+            if ($targetFolder) {
+                Write-Host "$($_.Name) -> $($folderMap[$key])" -ForegroundColor Green
+                Move-Item -Path $_.FullName -Destination $targetFolder -Force
+            }
         }
     }
-    catch {
-        
-    }
-    
-    if ($shouldMove) {
-        Move-Item -Path $_.FullName -Destination $schoolPath -Force
-    }
+    catch {}
 }
