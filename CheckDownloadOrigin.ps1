f2cf03ce77b46fe87ee1d79cb907d926dfd91b9f
Praise Akugbe <pakugbe@tegos.eu>
2026-02-04 13:38:30 +0100

    change

--- CheckDownloadOrigin.ps1
+++ CheckDownloadOrigin.ps1
@@ -11,21 +11,28 @@ $folderMap = @{
     'gitwbl' = 'GITWBL'
 }
 
+foreach ($folder in $folderMap.Values) {
+    $path = Join-Path $schoolPath $folder
+    if (-not (Test-Path $path)) {
+        New-Item -Path $path -ItemType Directory -Force | Out-Null
+    }
+}
+
 Get-ChildItem -Path $downloadsPath -File | ForEach-Object {
     try {
         $stream = Get-Content -Path $_.FullName -Stream Zone.Identifier -ErrorAction Stop
         $urls = ($stream | Select-String -Pattern '(HostUrl|ReferrerUrl)=(.*)$').Matches.Groups[2].Value
-        
+
         if ($urls | Where-Object { $url = $_; $schoolDomains | Where-Object { $url -like "$_*" } }) {
             $targetFolder = $null
-            
+
             foreach ($key in $folderMap.Keys) {
                 if ($urls -match $key) {
                     $targetFolder = Join-Path $schoolPath $folderMap[$key]
                     break
                 }
             }
-            
+
             if ($targetFolder) {
                 Write-Host "$($_.Name) -> $($folderMap[$key])" -ForegroundColor Green
                 Move-Item -Path $_.FullName -Destination $targetFolder -Force
@@ -34,3 +41,4 @@ Get-ChildItem -Path $downloadsPath -File | ForEach-Object {
     }
     catch {}
 }
+ 
